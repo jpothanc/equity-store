f475072fdc0724a48901360d100ddd317cab9112
jpothanc <70871841+jpothanc@users.noreply.github.com>
2024-03-24 10:32:27 +0800

    Update helper.py

--- server/internal/helper.py
+++ server/internal/helper.py
@@ -1,11 +1,21 @@
 import os
 
 def get_env_config_file():
-    env = os.getenv('FLASK_ENV', 'development')
+    env = os.getenv('FLASK_ENV', '')
+    # We should not default environments as it can lead to unexpected behavior
+    # but for ease of use we are defaulting to development
+    if env == '':
+        env = 'development'
+        # raise Exception("FLASK_ENV is not set")
     file_path = os.path.join("appsettings","config", f"config.{env}.json")
     return file_path
 
 def get_env_factory_file():
-    env = os.getenv('FLASK_ENV', 'development')
+    env = os.getenv('FLASK_ENV', '')
+    # We should not default environments as it can lead to unexpected behavior
+    # but for ease of use we are defaulting to development
+    if env == '':
+        env = 'development'
+        # raise Exception("FLASK_ENV is not set")
     file_path = os.path.join("appsettings","factory", f"factory.{env}.json")
     return file_path
